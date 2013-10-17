d52b46ed687763f0d1d77a89a80d354ec35df001
veleary <veleary@gmail.com>
2013-10-17 14:48:33 -0500

    update with destroy method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -74,8 +74,8 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
+      format.html { "Project was successfuly removed." }
+      format.js { render head: true }
     end
   end
 
